a3ebf48b362ad90b02395b6de6fa8f5a40d8cc77
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-04-06 20:25:13 -0300

    changed commans from 17 to 20

--- 19_type_constraints_list/commands.sh
+++ 19_type_constraints_list/commands.sh
@@ -1,12 +1,14 @@
 # command to start terraform
 terraform init
 
-# This script executes the `terraform apply` command with specific variable values passed via the `-var` flag.
-# - `bucket_name`: Specifies the name of the S3 bucket to be created or managed.
-# - `bucket_suffix`: Adds a suffix to the bucket name for differentiation or organization purposes.
-# Usage:
-# Run this script to apply the Terraform configuration with the provided variable values.
-terraform apply -var bucket_name=kevholditch -var bucket_suffix=foo
+# command to apply terraform and create resources on AWS
+# This command will create the resources defined in the main.tf file
+# It will prompt for confirmation before proceeding with the creation
+# If you want to skip the confirmation prompt, you can use the -auto-approve flag
+# terraform apply -auto-approve
+# This command will create the resources defined in the main.tf file
+# It will prompt for confirmation before proceeding with the creation
+terraform apply
 
 
 # command to destroy terraform and delete resources on AWS
