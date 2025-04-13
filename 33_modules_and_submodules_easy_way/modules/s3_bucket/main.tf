f8c5aa43e9fc0b840571f01c10f0a0a39dd6d6be
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-04-13 13:53:26 -0300

    Revert "32_modules_remote added command.sh"
    This reverts commit d5209e4b3584809657c5554cc6d10f0a37f65535.

--- 33_modules_and_submodules_easy_way/modules/s3_bucket/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "this" {
-  bucket = var.bucket_name
-
-  tags = {
-    Environment = "Dev"
-    ManagedBy   = "Terraform"
-  }
-}
--- 33_modules_and_submodules_easy_way/modules/s3_bucket/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_id" {
-  description = "The ID of the S3 bucket"
-  value       = aws_s3_bucket.this.id
-}
-
-output "bucket_name" {
-  description = "The name of the S3 bucket"
-  value       = aws_s3_bucket.this.bucket
-}
