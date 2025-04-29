a327b9e9b36bf0cafab4d1961ee127b36ce5e5a1
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-04-28 21:42:22 -0300

    542_workspaces Enhance comments for SQS queue naming based on workspace

--- 42_workspaces/main.tf
+++ 42_workspaces/main.tf
@@ -3,5 +3,10 @@ provider "aws" {
 }
 
 resource "aws_sqs_queue" "queue" {
+    # Create a queue with a name based on the workspace, e.g., "dev-queue", "prod-queue"
+    # This allows you to have different queues for different environments
+    # without having to change the code.
+    # The queue name will be prefixed with the workspace name.
+    # For example, if the workspace is "dev", the queue name will be "dev-queue".
     name = "${terraform.workspace}-queue"
 }
