7028e5fd9148b69aa72844440f4f5ef619d5fdab
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-04-29 21:16:11 -0300

    43_provisioners_introduction Refactor AWS instance provisioning to use user_data for improved efficiency and security

--- 43_provisioners_introduction/main.tf
+++ 43_provisioners_introduction/main.tf
@@ -133,3 +133,32 @@ resource "aws_instance" "nginx" {
         private_key = file("nginx_key")
     }
 }
+
+
+# Better solution using user_data instead of remote-exec provisioner
+# user_data is a script that will be executed when the instance is created
+# it is a managed service by AWS and it is free to use
+# it is a template that contains the commands that will be executed when the instance is created
+# it is a good practice to use user_data instead of remote-exec provisioner
+# because it is more efficient and faster
+# and it is easier to manage and maintain
+# and it is more secure because it does not require SSH access to the instance
+
+# resource "aws_instance" "nginx" {
+#   ami = data.aws_ami.ami.image_id
+#   instance_type = "t2.micro"
+#   subnet_id = aws_subnet.public.id
+#   vpc_security_group_ids = [aws_security_group.rules.id]
+#   key_name = aws_key_pair.keypair.key_name
+#   user_data = <<EOF
+#     #!/bin/bash
+#     set -ex
+#     yum update -y
+#     amazon-linux-extras enable nginx1.12
+#     yum -y install nginx
+#     chmod 777 /usr/share/nginx/html/index.html
+#     echo "Hello from nginx on AWS" > /usr/share/nginx/html/index.html
+#     systemctl start nginx
+    
+#   EOF
+# }
